0f8d8514459895048b7c92153eb7f6d9e78e1d93
David Hutchison <dhutchison86+github@googlemail.com>
2019-03-05 22:52:30 +0000

    Initial basic working code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { TreeModule } from 'primeng/tree';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileChooserComponent } from './file-chooser/file-chooser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileChooserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+
+    TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
