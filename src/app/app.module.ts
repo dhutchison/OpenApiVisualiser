6b35e43bb12c8ce10279f83bb475a1d0e04b9ea4
Andrew Duffy <a.duffy31@gmail.com>
2019-03-29 18:25:25 +0000

    Refactoring of project structure to introduce component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,12 @@ import { TreeModule } from 'primeng/tree';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FileChooserComponent } from './input-source/file-chooser/file-chooser.component';
-import { ApiPathTreeComponent } from './api-path-tree/api-path-tree.component';
-import { NodeMethodDetailComponent } from './node-method-detail/node-method-detail.component';
-import { ApiInformationComponent } from './api-information/api-information.component';
-import { UrlChooserComponent } from './input-source/url-chooser/url-chooser.component';
-import { InputSourceComponent } from './input-source/input-source.component';
+import { FileChooserComponent } from './components/input-source/file-chooser/file-chooser.component';
+import { ApiPathTreeComponent } from './components/api-path-tree/api-path-tree.component';
+import { NodeMethodDetailComponent } from './components/node-detail/node-method-detail/node-method-detail.component';
+import { ApiInformationComponent } from './components/api-information/api-information.component';
+import { UrlChooserComponent } from './components/input-source/url-chooser/url-chooser.component';
+import { InputSourceComponent } from './components/input-source/input-source.component';
 
 @NgModule({
   declarations: [
