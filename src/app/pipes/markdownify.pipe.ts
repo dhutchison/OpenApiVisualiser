a5d23b6ffab2a83cb8d6a2e139c7cca7046039ba
David Hutchison <dhutchison86+github@googlemail.com>
2019-10-19 23:23:47 +0100

    Fixed issue with undefined text in to pipes

--- src/app/pipes/markdownify.pipe.ts
+++ src/app/pipes/markdownify.pipe.ts
@@ -10,6 +10,13 @@ import { Parser, HtmlRenderer } from 'commonmark';
 export class MarkdownifyPipe implements PipeTransform {
 
   transform(value: string, args?: any): string {
+
+    if (value === undefined) {
+      /* cannot pass an undefined value into the parser, 
+       * at least set it to something. */
+      value = '';
+    }
+
     /* Parse the description in to the AST */
     const parser = new Parser({smart: true});
     const parsedValue = parser.parse(value);
