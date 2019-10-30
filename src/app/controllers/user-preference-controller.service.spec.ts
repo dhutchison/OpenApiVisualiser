63eae3edac67eeaa13ff5561d8b5f57f455d9fa0
David Hutchison <dhutchison86+github@googlemail.com>
2019-10-30 21:30:14 +0000

    Increased test coverage

--- src/app/controllers/user-preference-controller.service.spec.ts
+++ src/app/controllers/user-preference-controller.service.spec.ts
@@ -3,10 +3,26 @@ import { TestBed } from '@angular/core/testing';
 import { UserPreferenceControllerService } from './user-preference-controller.service';
 
 describe('UserPreferenceControllerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: UserPreferenceControllerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(UserPreferenceControllerService);
+  });
 
   it('should be created', () => {
-    const service: UserPreferenceControllerService = TestBed.get(UserPreferenceControllerService);
     expect(service).toBeTruthy();
   });
+
+  it('Default value should be set', () => {
+    expect(service.horizontalView).toBeTruthy();
+  });
+
+  it('Value should change', () => {
+    service.horizontalView = false;
+    expect(service.horizontalView).toBeFalsy();
+  });
+
+
 });
