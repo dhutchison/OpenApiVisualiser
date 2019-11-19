b6371da09411576ed9ebde8bde4fdf11fdbc900b
David Hutchison <dhutchison86+github@googlemail.com>
2019-11-19 19:01:39 +0000

    Initial commit of image save feature

--- src/app/components/api-path-tree/api-path-tree.component.ts
+++ src/app/components/api-path-tree/api-path-tree.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { TreeNode } from 'primeng/api';
 import { FileReaderService } from '../../services/file-reader.service';
 import { OpenapiTreenodeConverterService } from '../../services/openapi-treenode-converter.service';
 import { UserPreferenceControllerService } from 'src/app/controllers/user-preference-controller.service';
+
+
+import { toBlob, OptionsType } from 'html-to-image';
 
 @Component({
   selector: 'app-api-path-tree',
@@ -15,6 +18,9 @@ export class ApiPathTreeComponent implements OnInit {
    */
   apiPathNodes: TreeNode[] = [];
   selectedNode: TreeNode;
+
+  /* DOM element holding the API tree view */
+  @ViewChild('treeView', {static: false}) treeViewElement: ElementRef;
 
   constructor(
     private preferenceService: UserPreferenceControllerService,
@@ -46,5 +52,32 @@ export class ApiPathTreeComponent implements OnInit {
     this.selectedNode = undefined;
     /* Change the view */
     this.preferenceService.horizontalView = value;
+  }
+
+  /**
+   * Download an image of the API tree.
+   *
+   * This depends on the html-to-image library to do all the heavy work.
+   * https://github.com/bubkoo/html-to-image
+   *
+   * TODO: async / progress indicator (can take a while)
+   */
+  downloadImage() {
+
+    const exportOptions: OptionsType = {
+      backgroundColor: '#ffffff',
+      style: {
+        border: 0,
+        overflow: 'visible'
+      }
+    };
+
+    console.log(this.treeViewElement.nativeElement);
+
+    toBlob(this.treeViewElement.nativeElement, exportOptions)
+      .then(blob => {
+        window.saveAs(blob, 'API.png');
+    });
+
   }
 }
