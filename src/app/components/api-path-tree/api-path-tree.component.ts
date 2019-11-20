25e738d1a2132c306a846d8d563828ebf2cadeb0
David Hutchison <dhutchison86+github@googlemail.com>
2019-11-20 00:10:20 +0000

    api tree component UI tweaks

--- src/app/components/api-path-tree/api-path-tree.component.ts
+++ src/app/components/api-path-tree/api-path-tree.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { TreeNode } from 'primeng/api';
+import { TreeNode, SelectItem } from 'primeng/api';
 import { FileReaderService } from '../../services/file-reader.service';
 import { OpenapiTreenodeConverterService } from '../../services/openapi-treenode-converter.service';
 import { UserPreferenceControllerService } from 'src/app/controllers/user-preference-controller.service';
@@ -18,6 +18,15 @@ export class ApiPathTreeComponent implements OnInit {
    */
   apiPathNodes: TreeNode[] = [];
   selectedNode: TreeNode;
+
+  /* Boolean holding the state on if an image generation is in progress */
+  generatingImage = false;
+
+  /* Possible display types */
+  readonly viewTypes: SelectItem[] = [
+    {title: 'Tree', value: true, icon: 'pi pi-sitemap icon-rotate-ccw-90'},
+    {title: 'List', value: false, icon: 'pi pi-list'}
+  ];
 
   /* DOM element holding the API tree view */
   @ViewChild('treeView', {static: false}) treeViewElement: ElementRef;
@@ -64,6 +73,10 @@ export class ApiPathTreeComponent implements OnInit {
    */
   downloadImage() {
 
+    /* Set marker that an image is being generated */
+    this.generatingImage = true;
+
+    /* Configure the export */
     const exportOptions: OptionsType = {
       backgroundColor: '#ffffff',
       style: {
@@ -72,11 +85,15 @@ export class ApiPathTreeComponent implements OnInit {
       }
     };
 
+    /* Do the work. This uses a promise to async the work */
     console.log(this.treeViewElement.nativeElement);
 
     toBlob(this.treeViewElement.nativeElement, exportOptions)
       .then(blob => {
         window.saveAs(blob, 'API.png');
+
+        /* Mark that the generation is complete */
+        this.generatingImage = false;
     });
 
   }
