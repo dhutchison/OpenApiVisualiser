f80a8e81a698492a4afc4ab8dc5a5bf63b1c6beb
David Hutchison <dhutchison86+github@googlemail.com>
2019-12-05 21:57:16 +0000

    Initial commit, ugly but basically working

--- src/app/components/api-information/api-information.component.ts
+++ src/app/components/api-information/api-information.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FileReaderService } from '../../services/file-reader.service';
-import { OpenAPIObject } from 'openapi3-ts';
+import { getPath, OpenAPIObject, PathsObject, OperationObject } from 'openapi3-ts';
 
 @Component({
   selector: 'app-api-information',
@@ -8,15 +8,35 @@ import { OpenAPIObject } from 'openapi3-ts';
 })
 export class ApiInformationComponent implements OnInit {
 
-  apiDefinitions: OpenAPIObject[] = [];
+  apiDefinitions: Api[] = [];
+
+  /**
+   * Array containing the possible HTTP methods which can have operations for a path.
+   */
+  private readonly httpMethods = [
+    'get',
+    'put',
+    'post',
+    'delete',
+    'options',
+    'head',
+    'patch',
+    'trace'
+  ];
 
   constructor(
     private fileReaderService: FileReaderService) { }
 
   ngOnInit() {
     this.fileReaderService.apiChanged.subscribe(value => {
-      /* Add this specification to our current state */
-      this.apiDefinitions.push(value);
+      /* Add this specification to our current state,
+       * after calculating the statistics */
+      const api: Api = {
+        apiDefinition: value,
+        statistics: this.getApiStatistics(value)
+      };
+
+      this.apiDefinitions.push(api);
 
       console.log(value);
     });
@@ -27,4 +47,74 @@ export class ApiInformationComponent implements OnInit {
     });
   }
 
+  private getApiStatistics(api: OpenAPIObject): ApiStatistics {
+
+    /* Setup the base object */
+    const apiStats: ApiStatistics = {
+      pathCount: Object.keys(api.paths).length,
+      methodCount: 0,
+      methodsSplit: {
+        get: 0,
+        put: 0,
+        post: 0,
+        delete: 0,
+        options: 0,
+        head: 0,
+        patch: 0,
+        trace: 0
+      }
+    };
+
+    /* Iterate through each path and method */
+    Object.keys(api.paths).forEach(pathKey => {
+      const pathObject = getPath(api.paths, pathKey);
+
+      /* Iterate through the possible http methods, adding nodes as required */
+      this.httpMethods.forEach(method => {
+        if (pathObject[method]) {
+          /* Definition exists for the http method */
+          apiStats.methodsSplit[method]++;
+          apiStats.methodCount++;
+        }
+      });
+    });
+
+    return apiStats;
+
+  }
+
+  private calculateComplexity(operation: OperationObject): number {
+    //TODO: Implement;
+
+    /* Calculate the complexity of the request object */
+    // if (operation.requestBody instanceof ReferenceObject) {
+      /* Need to resolve the object */
+    // }
+    // operation.requestBody;
+
+    return 0;
+  }
+
+}
+
+interface ApiStatistics {
+
+  pathCount: number;
+  methodCount: number;
+  methodsSplit: {
+    get: number;
+    put: number;
+    post: number;
+    delete: number;
+    options: number;
+    head: number;
+    patch: number;
+    trace: number
+  };
+
+}
+
+interface Api {
+  apiDefinition: OpenAPIObject;
+  statistics: ApiStatistics;
 }
