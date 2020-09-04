74f58d52be4ffe6ee7f7f6fee059ea08bced5076
David Hutchison <dhutchison86+github@googlemail.com>
2020-09-04 19:15:31 +0100

    feat: added code coverage for cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// Import cypress code-coverage collector plugin
+import '@cypress/code-coverage/support';
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
