16280ad1dbd7ecd745a3bc6cf7466abe9e228b0e
David Hutchison <dhutchison86+github@googlemail.com>
2020-09-15 20:54:50 +0100

    fix: removed duplication in test

--- src/app/components/file-chooser/file-chooser.component.spec.ts
+++ src/app/components/file-chooser/file-chooser.component.spec.ts
@@ -5,6 +5,27 @@ import { FileUploadModule } from 'primeng/fileupload';
 import { FileReaderService } from '../../services/file-reader.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+/**
+ * Helper function for checking if a file extension is accepted or not
+ * @param component the component under test
+ * @param loadFileSpy the load file spy to check if it was called or not.
+ * @param filename the filename to check if accepted
+ * @param allowed boolean indicating if the filename should be accepted (true) or not (false)
+ */
+function checkFileExtensionAccepted(component: FileChooserComponent, loadFileSpy: jasmine.Spy, filename: string, allowed: boolean) {
+  const testFiles: File[] = [new File([], filename)];
+  component.loadFile(
+    {
+      files: testFiles
+    });
+
+  if (allowed) {
+    expect(loadFileSpy.calls.count()).toBe(1, 'spy method was called once');
+  } else {
+    expect(loadFileSpy.calls.count()).toBe(0, 'spy method was not called');
+  }
+}
+
 describe('FileChooserComponent', () => {
   let component: FileChooserComponent;
   let fixture: ComponentFixture<FileChooserComponent>;
@@ -50,38 +71,15 @@ describe('FileChooserComponent', () => {
 
   describe('Valid Inputs', () => {
     it('JSON file extension accepted', () => {
-
-      const testFiles: File[] = [new File([], 'input.json')];
-      component.loadFile(
-        {
-          files: testFiles
-        });
-
-      expect(loadFileSpy.calls.count()).toBe(1, 'spy method was called once');
+      checkFileExtensionAccepted(component, loadFileSpy, 'input.json', true);
     });
 
     it('YAML file extension accepted, as a target', () => {
-      const testFiles: File[] = [new File([], 'input.yaml')];
-      component.loadFile(
-        {
-          target: {
-            files: testFiles
-          }
-        });
-
-      expect(loadFileSpy.calls.count()).toBe(1, 'spy method was called once');
+      checkFileExtensionAccepted(component, loadFileSpy, 'input.yaml', true);
     });
 
     it('YAML "yml" file extension accepted, as a target', () => {
-      const testFiles: File[] = [new File([], 'input.yml')];
-      component.loadFile(
-        {
-          target: {
-            files: testFiles
-          }
-        });
-
-      expect(loadFileSpy.calls.count()).toBe(1, 'spy method was called once');
+      checkFileExtensionAccepted(component, loadFileSpy, 'input.yml', true);
     });
 
     it('Multiple files accepted', () => {
@@ -102,13 +100,7 @@ describe('FileChooserComponent', () => {
 
   describe('Invalid Inputs', () => {
     it('txt file extension rejected', () => {
-      const testFiles: File[] = [new File([], 'input.txt')];
-      component.loadFile(
-        {
-          files: testFiles
-        });
-
-      expect(loadFileSpy.calls.count()).toBe(0, 'spy method was not called');
+      checkFileExtensionAccepted(component, loadFileSpy, 'input.txt', false);
     });
   });
 });
